8cc353d1469c254352b48720eb62368633ab3379
Jing Palma <jingpalma17@gmail.com>
2020-12-24 06:24:06 +0800

    Implemented todo-list component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+        <li v-for="(todoList, idx) in TodoList" :key="idx">
+            <span>{{todoList.name}}</span><button @click="Delete(idx)">x</button>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+      TodoList: Array,
+      Delete: Function
+  }
+}
+</script>
+
+<style>
+</style>
